d719c26bbe30d631f4e324d74edfaee570e6efa2
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2023-02-07 16:41:37 +0100

    added goal as args to distinguish type of run and fruther carved out modeling to training_functions

--- main.py
+++ main.py
@@ -4,36 +4,22 @@ from pathlib import Path
 import pandas as pd
 
 from utils import store_submission
-from training_functions import load_train_data, train_mocov_features, predict_cv_classifiers
-from modeling.tabular_models import get_tabular_estimator
+from training_functions import train_tabular
 
 ###############################################################################
 
 parser = argparse.ArgumentParser(description = 'Main Script to Run Training')
-parser.add_argument('--model', type = str, default = r"XGB", help = 'Type of Model to Run Training')
+parser.add_argument('--model', type = str, default = r"XGB", help = 'Type of model to run training')
+parser.add_argument('--goal', type = str, default = r"test", help = 'Goal of the run (test/submission/tuning)')
 args = parser.parse_args()  
 
 ###############################################################################
 
 if __name__ == "__main__":
 
-    model = vars(args)["model"]
-    estimator = get_tabular_estimator(model)
+    input_args = vars(args)
 
-    data_path = Path("./storage")
-    output = pd.read_csv(data_path / "train_output.csv")
-    md_train = pd.read_csv(data_path / "supplementary_data" / "train_metadata.csv")
-    output_md = md_train.merge(output, on="Sample ID")
+    preds = train_tabular(input_args["model"]) 
 
-    (
-        X_train,
-        y_train,
-        patients_unique,
-        y_unique,
-        patients_train,
-    ) = load_train_data(output_md)
-    lrs = train_mocov_features(
-        estimator, X_train, y_train, patients_unique, y_unique, patients_train
-    )
-    preds = predict_cv_classifiers(lrs)  
-    store_submission(preds, "test")
+    if input_args["goal"] == "submission":
+        store_submission(preds, "test")
