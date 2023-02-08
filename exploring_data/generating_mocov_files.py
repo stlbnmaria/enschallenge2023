9ae83139ad40157a2deb2594b1e755abf0846583
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2023-02-08 13:59:00 +0100

    added function to write test mocov features

--- exploring_data/generating_mocov_files.py
+++ exploring_data/generating_mocov_files.py
@@ -4,7 +4,7 @@ from pathlib import Path
 from tqdm import tqdm
 
 
-def write_mocov_csv(data_path=Path("../storage")):
+def write_mocov_train(data_path=Path("../storage")):
     """
     This function loads the MoCov features for training and stores one
     compressed numpy file incl. all tile values.
@@ -48,5 +48,46 @@ def write_mocov_csv(data_path=Path("../storage")):
     print("------------------- File saved successfully -------------------")
 
 
+def write_mocov_test(data_path=Path("../storage")):
+    """
+    This function loads the MoCov features for training and stores one
+    compressed numpy file incl. all tile values.
+    """
+    metadata = pd.read_csv(data_path / "supplementary_data" / "test_metadata.csv")
+
+    features_dir = data_path / "test_input" / "moco_features"
+
+    X_train = np.empty([0, 2048])
+    centers_train = np.empty([])
+    patients_train = np.empty([])
+    samples_train = np.empty([])
+
+    for sample, center, patient in tqdm(
+        metadata[["Sample ID", "Center ID", "Patient ID"]].values
+    ):
+        # load the coordinates and features (1000, 3+2048)
+        _features = np.load(features_dir / sample)
+        # get coordinates (zoom level, tile x-coord on the slide, tile y-coord on the slide)
+        # and the MoCo V2 features
+        _, features = _features[:, :3], _features[:, 3:]  # Ks
+
+        n = features.shape[0]
+        X_train = np.vstack((X_train, features))
+        centers_train = np.hstack((centers_train, [center] * n))
+        patients_train = np.hstack((patients_train, [patient] * n))
+        samples_train = np.hstack((samples_train, [sample] * n))
+
+    # store csv
+    output = np.vstack((patients_train[1:], samples_train[1:], centers_train[1:])).T
+    np.savez_compressed(
+        data_path / "test_input" / "mocov_features_test",
+        metadata=output,
+        features=X_train,
+    )
+
+    print("------------------- File saved successfully -------------------")
+
+
 if __name__ == "__main__":
-    write_mocov_csv()
+    write_mocov_train()
+    write_mocov_test()
