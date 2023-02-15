a62e7a6107c47ea89c6bbf7ab62b176b81b328b4
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2023-02-15 21:31:20 +0100

    added catboost to trainign pipeline

--- modeling/tabular_models.py
+++ modeling/tabular_models.py
@@ -1,12 +1,19 @@
 from xgboost import XGBClassifier
+from catboost import CatBoostClassifier
 
 
 def xgb_estimator():
     estimator = XGBClassifier(colsample_bylevel=0.2)
     return estimator
 
+def catboost_estimator():
+    estimator = CatBoostClassifier(colsample_bylevel=0.2)
+    return estimator
+
 def get_tabular_estimator(model: str):
     if model == "XGB":
         return xgb_estimator()
+    elif model == "Catboost":
+        return catboost_estimator()
     else:
         print("The specified model is not valid")
