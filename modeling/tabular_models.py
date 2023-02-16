f953b38bc88f3946491d2c6c47284b84b3fd1353
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2023-02-16 20:52:43 +0100

    added logistic regression as basline

--- modeling/tabular_models.py
+++ modeling/tabular_models.py
@@ -1,5 +1,11 @@
 from xgboost import XGBClassifier
 from catboost import CatBoostClassifier
+from sklearn.linear_model import LogisticRegression
+
+
+def logreg_estimator():
+    estimator = LogisticRegression()
+    return estimator
 
 
 def xgb_estimator():
@@ -11,7 +17,9 @@ def catboost_estimator():
     return estimator
 
 def get_tabular_estimator(model: str):
-    if model == "XGB":
+    if model == "LogReg":
+        return logreg_estimator()
+    elif model == "XGB":
         return xgb_estimator()
     elif model == "Catboost":
         return catboost_estimator()
