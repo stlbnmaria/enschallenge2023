f4669f42c18101d3ab5753d39512baeb234a2e6e
Maria Stoelben <mariastoelben@MBP-von-Maria.fritz.box>
2023-02-27 10:29:10 +0100

    added RF and extratrees to estimators

--- modeling/tabular_models.py
+++ modeling/tabular_models.py
@@ -1,6 +1,7 @@
 from xgboost import XGBClassifier
 from catboost import CatBoostClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 
 
 def logreg_estimator():
@@ -12,13 +13,19 @@ def xgb_estimator():
     estimator = XGBClassifier()
     return estimator
 
+
 def catboost_estimator():
     estimator = CatBoostClassifier(colsample_bylevel=0.2)
     return estimator
 
+
 def get_tabular_estimator(model: str):
     if model == "LogReg":
         return logreg_estimator()
+    elif model == "RF":
+        return RandomForestClassifier(n_jobs=4)
+    elif model == "ET":
+        return ExtraTreesClassifier()
     elif model == "XGB":
         return xgb_estimator()
     elif model == "Catboost":
