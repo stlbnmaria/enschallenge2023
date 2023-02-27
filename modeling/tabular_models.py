18bbd5b03ba6017d35493b32526d4a0d0cb210b0
Maria Stoelben <mariastoelben@MBP-von-Maria.fritz.box>
2023-02-27 11:27:37 +0100

    restructered estimator return

--- modeling/tabular_models.py
+++ modeling/tabular_models.py
@@ -1,34 +1,27 @@
-from xgboost import XGBClassifier
 from catboost import CatBoostClassifier
+from lce import LCEClassifier
+from lightgbm import LGBMClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
-
-
-def logreg_estimator():
-    estimator = LogisticRegression()
-    return estimator
-
-
-def xgb_estimator():
-    estimator = XGBClassifier()
-    return estimator
-
-
-def catboost_estimator():
-    estimator = CatBoostClassifier(colsample_bylevel=0.2)
-    return estimator
+from sklearn.tree import DecisionTreeClassifier
+from xgboost import XGBClassifier
 
 
 def get_tabular_estimator(model: str):
-    if model == "LogReg":
-        return logreg_estimator()
-    elif model == "RF":
-        return RandomForestClassifier(n_jobs=4)
-    elif model == "ET":
-        return ExtraTreesClassifier()
-    elif model == "XGB":
-        return xgb_estimator()
-    elif model == "Catboost":
-        return catboost_estimator()
-    else:
+    # define dict of potential estimators
+    estimators = {
+        "LogReg": LogisticRegression(),
+        "DT": DecisionTreeClassifier(),
+        "RF": RandomForestClassifier(),
+        "ExtraTrees": ExtraTreesClassifier(),
+        "XGB": XGBClassifier(),
+        "Catboost": CatBoostClassifier(verbose=0), 
+        "LightGBM": LGBMClassifier(), 
+        "LCE": LCEClassifier(),      
+    }
+
+    # get estimator based on specified specified model
+    try:
+        estimator = estimators[model]
+    except:
         print("The specified model is not valid")
