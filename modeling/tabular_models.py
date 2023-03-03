5df789eb669d1fcb418c2d5cee4f1f9aaee07533
Maria Stoelben <mariastoelben@MBP-von-Maria.fritz.box>
2023-03-03 10:44:11 +0100

    added class weighting to RF and LogReg

--- modeling/tabular_models.py
+++ modeling/tabular_models.py
@@ -2,6 +2,7 @@ from catboost import CatBoostClassifier
 from lightgbm import LGBMClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 
@@ -9,13 +10,14 @@ from xgboost import XGBClassifier
 def get_tabular_estimator(model: str):
     # define dict of potential estimators
     estimators = {
-        "LogReg": LogisticRegression(),
+        "LogReg": LogisticRegression(class_weight={0:.35, 1:.65}),
         "DT": DecisionTreeClassifier(),
-        "RF": RandomForestClassifier(),
-        "ExtraTrees": ExtraTreesClassifier(),
+        "RF": RandomForestClassifier(max_features=1, n_jobs=6, class_weight={0:.1, 1:.9}),
+        "ExtraTrees": ExtraTreesClassifier(n_jobs=6),
         "XGB": XGBClassifier(),
         "Catboost": CatBoostClassifier(verbose=0), 
         "LightGBM": LGBMClassifier(),  
+        "MLP": MLPClassifier(),
     }
 
     # get estimator based on specified specified model
