7f042304d79363c0e1d0c23d1bc1c7e5588f8bf9
Maria Stoelben <mariastoelben@MacBook-Pro-von-Maria.local>
2023-03-07 14:29:57 +0100

    added function to read paramter grid from modeling input xlsx

--- modeling/tabular_models.py
+++ modeling/tabular_models.py
@@ -1,5 +1,9 @@
+import os
+
+from ast import literal_eval
 from catboost import CatBoostClassifier
 from lightgbm import LGBMClassifier
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier, ExtraTreesClassifier
 from sklearn.neural_network import MLPClassifier
@@ -7,12 +11,29 @@ from sklearn.tree import DecisionTreeClassifier
 from xgboost import XGBClassifier
 
 
+def read_grid_tuning(path: str = "./modeling/"):
+    """
+    This function loads the modeling inputs from a predefined excel input,
+    inlc. the feature engineering selection inputs and
+    hyperparameter grid.
+    """
+    modeling_inputs = pd.read_excel(os.path.join(path, "modeling_inputs.xlsx"))
+    # extract hyperparameter grid
+    grid = modeling_inputs["grid"][0]
+    try:
+        grid = literal_eval(grid)
+    except:
+        grid = None
+    
+    return grid
+
+
 def get_tabular_estimator(model: str):
     # define dict of potential estimators
     estimators = {
-        "LogReg": LogisticRegression(),
+        "LogReg": LogisticRegression(solver="newton-cholesky"),
         "DT": DecisionTreeClassifier(),
-        "RF": RandomForestClassifier(n_jobs=6),
+        "RF": RandomForestClassifier(n_jobs=6, n_estimators=300),
         "ExtraTrees": ExtraTreesClassifier(n_jobs=6),
         "XGB": XGBClassifier(),
         "Catboost": CatBoostClassifier(verbose=0), 
